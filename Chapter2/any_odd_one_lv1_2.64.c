718e2a9b42211ede96246b69725482668bbcb28a
wangpengjun <742874584@qq.com>
2019-02-15 21:24:55 +0800

    2.64题修正：位数是从0开始数的

--- Chapter2/any_odd_one_lv1_2.64.c
+++ Chapter2/any_odd_one_lv1_2.64.c
@@ -4,15 +4,15 @@
 int any_odd_one(unsigned x)
 {
     /* 如题 假设int有32位 */
-    int mask = 0x55555555;
+    int mask = 0xAAAAAAAA;
 
     return !!(x & mask);
 }
 
 int main(int argc, char* argv[])
 {
-    unsigned test_odd = 0x1;
-    unsigned test_no_odd = 0x2;
+    unsigned test_odd = 0x2;
+    unsigned test_no_odd = 0x1;
 
     assert(any_odd_one(test_odd) == 1);
     assert(any_odd_one(test_no_odd) == 0);
