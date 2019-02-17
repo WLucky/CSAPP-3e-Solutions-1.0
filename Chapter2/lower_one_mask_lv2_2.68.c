b96fc81c8e57f20bb50e7cf39fdc2efc9e7ec056
wangpengjun <742874584@qq.com>
2019-02-17 22:21:44 +0800

    2.68

---
+++ Chapter2/lower_one_mask_lv2_2.68.c
@@ -0,0 +1,17 @@
+#include<assert.h>
+
+
+int lower_one_mask(int n)
+{
+    int x = 1 << n;
+
+    return (x - 1);
+}
+
+int main(int argc, char *argv[])
+{
+    assert(lower_one_mask(6) == 0x3F);
+    assert(lower_one_mask(17) == 0x1FFFF);
+
+    return 0;
+}
