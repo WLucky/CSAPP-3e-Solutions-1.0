68f2b723d8fc8d55893e2f1bdf08415811e565b3
wangpengjun <742874584@qq.com>
2019-02-17 22:07:18 +0800

    2.67 (参考了答案 自己做了后一半)

---
+++ Chapter2/int_size_is_32_lv2_2.67.c
@@ -0,0 +1,30 @@
+#include <assert.h>
+
+/*
+A.左移不能超过最大位数？ 引发不可知行为？ yes
+
+*/
+
+int int_size_is_32()
+{
+    int set_msb = 1 << 31;
+    int beyond_msb = set_msb << 1;
+
+    return set_msb && !beyond_msb;
+}
+
+int int_size_is_32_v2()
+{
+    int set_msb = 1 << 16 << 15;
+    int beyond_msb = set_msb << 1;
+
+    return set_msb && !beyond_msb;
+}
+
+int main(int argc, char *argv[])
+{
+    assert(int_size_is_32());
+    assert(int_size_is_32_v2());
+
+    return 0;
+}
