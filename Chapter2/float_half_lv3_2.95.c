39c6378a321499ba5e950e80160423d347af431b
wangpengjun <742874584@qq.com>
2019-03-17 20:50:41 +0800

    2.95 ver2

--- Chapter2/float_half_lv3_2.95.c
+++ Chapter2/float_half_lv3_2.95.c
@@ -28,51 +28,49 @@ float_bits float_half(float_bits f)
     unsigned exp = f >> 23 & 0xFF;
     unsigned frac = f & 0x7FFFFF;
 
-    if(exp == 0)
+    /* 
+    右移一位向偶数舍入的情况：
+    exp = 0或者1 且最低两位为11时，加1 
+    */
+    if (exp <= 1)
     {
-        if((frac & 0x3) == 0x3)
+        if ((frac & 0x3) == 0x3)
         {
             frac += 1;
         }
+    }
 
+    if (exp == 0)
+    {
         frac >>= 1;
     }
-    else if(exp < 0xFF)
+    else if (exp == 1)
     {
-        if(exp > 1)
+        if ((frac >> 23) == 0)
         {
-            exp -=1;
+            /* frac未溢出 则转换为非规格化数 */
+            exp -= 1;
+            frac = (frac >> 1) | (1 << 22);
         }
         else
         {
-            if((frac & 0x3) == 0x3)
-            {
-                frac = frac + 1;
-            }
-            
-            
-            if((frac >> 23) == 0)
-            {
-                /* frac未溢出 则转换为非规格化数 */
-                exp -=1;
-                frac = ((frac & 0x7FFFFF) >> 1) | (1 << 22);
-            }
-            else
-            {
-                /* frac溢出 则保持规格化 同时frac归零 */
-                exp = 1;
-                frac = 0;
-            }
+            /* frac溢出 则保持规格化 同时frac归零 */
+            exp = 1;
+            frac = 0;
         }
+    }
+    else if (exp < 0xFF)
+    {
+        exp -= 1;
     }
     else
     {
+        /* NaN或者oo 保持不变*/
         return f;
     }
 
     return (sign << 31) | (exp << 23) | frac;
 }
-
 
 int main(int argc, char *argv[])
 {
@@ -80,31 +78,15 @@ int main(int argc, char *argv[])
     float half_x;
     int count = 0;
 
-    for(unsigned i = 0; i < (unsigned)(-1); i++)
+    for (unsigned i = 0; i < (unsigned)(-1); i++)
     {
         x = *(float *)&i;
         half_x = 0.5 * x;
 
         /* for a float f, f != f will be true only if f is NaN. */
-        if(x == x)
+        if (x == x)
         {
-            if(float_half(i) != *(float_bits *)&half_x)
-            {
-                printf("\nNo.%d\n", count++);
-                
-                unsigned y = float_half(i);
-
-                show_unsigned(i);
-                show_unsigned(y);
-                show_unsigned(*(unsigned *)&half_x);
-
-                if(count == 10)
-                {
-                    return 0;
-                }
-            }
-
-            /*assert(float_half(i) == *(float_bits *)&half_x);*/
+            assert(float_half(i) == *(float_bits *)&half_x);
         }
     }
 
