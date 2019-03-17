3bc6bea9b10454f234d949ad71a5f819ded7562d
wangpengjun <742874584@qq.com>
2019-03-17 16:50:20 +0800

    2.94

--- Chapter2/float_twice_lv3_2.94.c
+++ Chapter2/float_twice_lv3_2.94.c
@@ -4,22 +4,76 @@
 
 typedef unsigned float_bits;
 
+/*
+exp == 0 非规格化：(E = 1 - bias = -126)
+    (frac >> 22) == 0:
+        frac << 1;
+    esle:
+        -->向规格化过度
+        exp += 1; (E = 1 - bais = -126)
+        frac = (frac << 1) & 0x7FFFFF;(相当于右移一位，形成1.a)
+else exp < 0xFF  规格化：
+        exp += 1;
+        如果溢出 则修改为 infinite格式
+        if(exp == 0xFF)
+        {
+            frac = 0;
+        }
+else exp == 0xFF
+        infinite和NaN的情况保持不变
+        return f;
+
+*/
 float_bits float_twice(float_bits f)
 {
+    unsigned sign = f >> 31;
+    unsigned exp = f >> 23 & 0xFF;
+    unsigned frac = f & 0x7FFFFF;
 
+    if(exp == 0)
+    {
+        if(frac >> 22 == 1)
+        {
+            exp += 1;
+        }
+
+        frac = (frac << 1) & 0x7FFFFF;
+    }
+    else if(exp < 0xFF)
+    {
+        exp += 1;
+        if(exp == 0xFF)
+        {
+            frac = 0;
+        }
+    }
+    else
+    {
+        return f;
+    }
+
+    return (sign << 31) | (exp << 23) | frac;
 }
 
 
 int main(int argc, char *argv[])
 {
-    /* 0 100000.. 1100.... ==> 40 60 00 00*/
-    unsigned x = 0x406000;
-    float y = *(float *)&x;
+    float x;
+    float twice_x;
+    int count = 0;
+    for(unsigned i = 0; i < (unsigned)(-1); i++)
+    {
+        x = *(float *)&i;
+        twice_x = 2.0 * x;
 
-    float twice_y = 2.0 * y;
+        /* for a float f, f != f will be true only if f is NaN. */
+        if(x == x)
+        {
+            assert(float_twice(i) == *(float_bits *)&twice_x);
+        }
+    }
 
-    printf("y: %f\n2y: %f\n", y, twice_y);
-    show_unsigned((unsigned *)&twice_y);
+
 
     return 0;
 }
